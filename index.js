c8fce89f56f116842622cdb16174a3550c667cc8
webkieth <firekit13@gmail.com>
2021-12-02 14:04:54 +0300

    fixed version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+import ease from 'easy-ease'
 export default {
   /**
    * install function
@@ -8,12 +8,31 @@ export default {
 
     Vue.directive('prlx', {
       bind (el, options) {
+        let canTransform = true
         window.addEventListener('scroll', () => {
-          if (!el) return false
-          requestAnimationFrame(() => {
-            el.style.transform = `translateY(${
-              Math.floor((window.pageYOffset - el.parentNode.offsetTop) / (options.value.delimeter || 2))
-            }px)`
+          if (!el || !canTransform) return false
+          const { transform } = window.getComputedStyle(el);
+          let currentStep = 0
+          if (transform && transform !== 'none') {
+            const matrixValues = transform.match(/matrix.*\((.+)\)/)[1].split(', ')
+            currentStep = matrixValues[5]
+          }
+          if (currentStep === 0) {
+            el.style.transform = `translateY(0px)`
+            return
+          }
+          const nextStep = Math.floor((window.pageYOffset - el.parentNode.offsetTop) / (options.value.delimeter || 2))
+          ease({
+            startValue: Number(currentStep),
+            endValue: nextStep,
+            durationMs: 10,
+            onStep: (value) => {
+              canTransform = false
+              requestAnimationFrame(() => {
+                el.style.transform = `translateY(${value}px)`
+              })
+            },
+            onComplete: () => canTransform = true
           })
         })
       }
